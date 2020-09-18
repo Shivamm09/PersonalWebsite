7c11410ea721671e8943196d109788c21cb55ddc
Shivamm09 <shivammgupta09@gmail.com>
2020-09-18 20:49:39 +0100

    Updates

---
+++ precache-manifest.0ef97d24902ef420ff6b5b91d10a138a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ab6f2aea631571a43f665719197793",
+    "url": "/PersonalWebsite/index.html"
+  },
+  {
+    "revision": "15382ca3ba18e07a4650",
+    "url": "/PersonalWebsite/static/css/main.d38777f7.chunk.css"
+  },
+  {
+    "revision": "b9e51770c6a240767ff8",
+    "url": "/PersonalWebsite/static/js/2.0a445569.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/PersonalWebsite/static/js/2.0a445569.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15382ca3ba18e07a4650",
+    "url": "/PersonalWebsite/static/js/main.d1b95484.chunk.js"
+  },
+  {
+    "revision": "ed09f7f4f63177ac5df1",
+    "url": "/PersonalWebsite/static/js/runtime-main.f1fb97d7.js"
+  },
+  {
+    "revision": "22e70043110570f88fd8ab34a794ee43",
+    "url": "/PersonalWebsite/static/media/happy.22e70043.JPG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PersonalWebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
